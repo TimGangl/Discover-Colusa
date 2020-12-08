e455d06c29f25a8565aae482e73939545ad7c49d
Jed Smit <jedsmit1@gmail.com>
2020-12-07 17:59:52 -0800

    refactor modal to businessPage component

--- src/components/CarouselModal/CarouselModal.js
+++ src/components/CarouselModal/CarouselModal.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
+// import Modal from 'react-bootstrap/Modal';
+import { Link } from 'react-router-dom';
 
 const CarouselModal = props => {
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  // function openInNewTab(url) {
+  //   var win = window.open(url, '_blank');
+  //   win.focus();
+  // }
+
   return (
-    <>
-      <img src={props.src} alt={props.alt} onClick={handleShow}></img>
-      <Modal show={show} onHide={handleClose}>
+    <Link
+      to={{
+        pathname: './business',
+        state: {
+          src: props.src,
+          alt: props.alt,
+          businessName: props.businessName,
+          text: props.text,
+          footer: props.footer,
+        },
+      }}
+    >
+      <img src={props.src} alt={props.alt}></img>
+      {/* <Modal show={show} onHide={handleClose}>
         <Modal.Header>
           <Modal.Title>{props.businessName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>{props.text}</Modal.Body>
         <Modal.Footer>{props.footer}</Modal.Footer>
-      </Modal>
-    </>
+      </Modal> */}
+    </Link>
   );
 };
 
