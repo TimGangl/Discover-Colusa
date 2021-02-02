abce176c45cd4e5f10e122f47490c0a7f15fd63d
Jed Smit <jedsmit1@gmail.com>
2021-02-01 18:18:29 -0800

    refactior blog pages to react-bootstrap, fix file tree issues

--- src/pages/BlogArticle/BlogArticle.js
+++ src/pages/BlogArticle/BlogArticle.js
@@ -4,32 +4,39 @@ import Footer from '../../components/Footer/Footer';
 import './BlogArticle.css';
 import photo from '../../images/blog.jpg';
 import singleBlogData from '../../data/BlogData';
-import BlogList from '../BlogSingle/BlogList';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 function BlogArticle(props) {
   const location = useLocation();
 
   const { title, author, date, preview, content, src, alt } = location.state;
+
   return (
-    <>
-      <div>
-        <div className='row'>
-          <div className='col-sm-2'></div>
-          <div className='col-8'>
-            <h1 classname='blogInfo'>{title}</h1>
-          </div>
-          <div className='col-sm-2'></div>
-          <div className='row'>
-            <div className='col-lg-2 col-md-0'></div>
-            <div classname='col-lg-4 col-md-6 mt-3'></div>
-            <div classname='col-lg-4 col-md-6 mt-3'></div>
-            <div className='col-lg-2 col-md-0'></div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
+    <Card>
+      <Card.Img
+        className='img-fluid mx-auto w-100'
+        src={props.src}
+        alt={props.alt}
+        variant='top'
+      />
+
+      <Card.Body>
+        <Card.Text>
+          <h2>{props.title}</h2>
+        </Card.Text>
+        <Card.Text>
+          <h4>By: {props.author}</h4>
+        </Card.Text>
+        <Card.Text>
+          <h4>Date: {props.date}</h4>
+        </Card.Text>
+
+        <h5 className='w-100'>
+          <strong>Preview: </strong>
+          {props.preview}
+        </h5>
+      </Card.Body>
+    </Card>
   );
 }
 
