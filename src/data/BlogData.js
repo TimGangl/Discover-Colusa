f1404728174f9387f06eaeb65b3936bcb332c830
Tim Gangl <tim.gangl30@gmail.com>
2021-02-06 16:41:43 -0800

    blog page done

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,42 +1,105 @@
 //import images for blogs
-import duck from '../images/BlogImages/duck.jpg';
-import burrito from '../images/cabana-burrito.jpeg';
+import ChinaTown from '../images/BlogImages/ChinaTown.jpg';
+import ColusaAerial from '../images/BlogImages/ColusaAerial.png';
+import LeveeParkRiver from '../images/BlogImages/LeveeParkRiver.jpg';
+import KellersThumb from '../images/BlogImages/kellehers-thumb.png';
+import GrandHouse from '../images/BlogImages/Grand-House.jpg';
 
 export const singleBlogData = [
   {
     images: [
       {
-        src: duck,
-        alt: 'a duck',
+        src: ChinaTown,
+        alt: 'Colusa China Town District',
       },
       {
-        src: burrito,
-        alt: 'a burrito',
+        src: ColusaAerial,
+        alt: 'Aerial Veiw of Colusa',
+      },
+      {
+        src: LeveeParkRiver,
+        alt: 'Image of the Levee Park River',
       },
     ],
-    blogTitle: 'Welcome To Discover Colusa',
-    blogAuthor: 'Tim Gangl',
-    blogDate: 'January 30, 2021',
+    blogTitle: 'Top 5 Instagram-Worthy Photos in Colusa',
+
+    blogAuthor: 'Kristy Levings',
+
+    blogDate: 'February 2021',
+
     blogPreview:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat libero et sodales eleifend. Donec enim libero, lacinia facilisis vestibulum vel, blandit eu risus.',
-    blogContent:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat libero et sodales eleifend. Donec enim libero, lacinia facilisis vestibulum vel, blandit eu risus. Morbi tristique feugiat nibh, malesuada porta quam facilisis in. Proin iaculis iaculis eros, quis imperdiet ex vehicula vel. Duis rutrum neque malesuada faucibus dignissim.',
+      'For those Northern Californians looking for a day trip near-cation, Colusa is photo competitive sure to surprise and delight leaving visitors with both images and memories.',
+
+    blogContent1:
+      "For those Northern Californians looking for a day trip near-cation, Colusa is photo competitive sure to surprise and delight leaving visitors with both images and memories. Here’s the Top 5 locations to capture inevitable Instagram envy from friends and family alike.",
+    blogContent2:
+      "1) Grand Houses of 10th Street: These grand dames of a Victorian past exude a history of posh gossip and whisper of wealth from a bygone era. Boasting many bedrooms, sky- soaring roofs, rounded turrets, Victorian gingerbread, wrap - around porches - these grand houses are nothing short of special.",
+
+    blogContent3:
+      "2) Levee Park: This long, thin park borders the roaring Sacramento River, a boating and fishing mecca.Viewers can visit the park any day of the week walking the curved pathway that follows the path of the river stretching from the City’s 10th street down to it’s Bridge Street. Families with picnics, birthday bar - b - ques and more are all welcome at Levee Park.",
+
+    blogContent4:
+      "3) Historic Chinese District on Main Street: Mirroring the Levee Park along the river is Main Street.Think a more historic version of Old Town Sacramento. Brick buildings from a bygone era with rumors of an underground network of tunnels connecting one shop to another. One of the most photogenic spots on a sunny day.",
+
+    blogContent5:
+      "4) Colusa Water Towers: These water towers can be seen from almost anyplace in the city and are a reminder of the City’s close connection to the River. Once labeled Hot and Cold, the towers stand 200 feet in the air and require regular annual cleaning by those not afraid of heights. You can take a selfie on Main St. with the Towers in the background.",
+
+    blogContent6:
+      "5) Colusa National Wildlife Refuge: Located just up the road from Colusa, the Wildlife Refuge is the place for orinthologists and bird enthusiasts of all types.Stop in Colusa and grab a snack before heading to the refuge for a day - time get away. 2180 State Hwy 20, Colusa.",
   },
   {
     images: [
       {
-        src: duck,
-        alt: 'a duck',
+        src: KellersThumb,
+        alt: 'Street in Colusa',
       },
     ],
     alt: 'a duck with crazy hair',
-    blogTitle: 'Another Blog about Colusa',
-    blogAuthor: 'Jed Smit',
-    blogDate: 'January 30, 2021',
+
+    blogTitle: 'How to Start a Business in Colusa in 3 Easy Steps',
+
+    blogAuthor: 'Kristy Levings',
+
+    blogDate: 'January 2021',
+
     blogPreview:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat libero et sodales eleifend. Donec enim libero, lacinia facilisis vestibulum vel, blandit eu risus.',
-    blogContent:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat libero et sodales eleifend. Donec enim libero, lacinia facilisis vestibulum vel, blandit eu risus. Morbi tristique feugiat nibh, malesuada porta quam facilisis in. Proin iaculis iaculis eros, quis imperdiet ex vehicula vel. Duis rutrum neque malesuada faucibus dignissim.',
+      'Starting a business in Colusa is as easy as one two three. Just follow these three easy steps!',
+
+    blogContent1:
+      'Starting a business in Colusa is as easy as one two three. Just follow these three easy steps!',
+
+    blogContent2:
+      '1.	Find a location that fits your business: If you have a manufacturing business, then the Colusa Industrial Park or the Colusa BioInnovation Center might be a good fit. If you have a retail or restaurant business catering to tourists or residents, then the Historic Riverfront District or vibrant Fremont Street business districts might be a good place for you. You can explore open commercial sites on this interactive map at the City website: www.cityofcolusa.com/home/business',
+
+    blogContent3:
+      '2)	Work with the City: The City of Colusa offers substantial services for new businesses including help with site selection and negotiating with landlords. The City has access to unique funding designed to help new businesses, and can help get employees plugged in. The City also works with new start-ups of business plan development and even marketing and promotion. (530) 393-6843 or email: cbic(@)cityofcolusa.com',
+
+    blogContent4:
+      '3) Get your Business Permit: The City of Colusa business permit fees are low and based on how much you expect to make that year.You can access it online at: www.cityofcolusa.com/home/business/permits-licenses/ Or, you can visit the City offices at 425 Webster Street and grab an application from the friendly admin people at the front desk!',
+  },
+  {
+    images: [
+      {
+        src: GrandHouse,
+        alt: 'The Grand House',
+      }
+    ],
+
+    blogTitle: 'Grand House of Colusa: The Thomas Rutledge House at 409 Tenth Street',
+
+    blogAuthor: 'Kristy Levings',
+
+    blogDate: 'January 2021',
+
+    blogPreview:
+      'This two-story abode was first constructed in 1905 by Thomas Rutledge and designed in the turn-of-the century transitional style, incorporating both Queen Anne and Colonial Revival.',
+
+    blogContent1:
+      'This two-story abode was first constructed in 1905 by Thomas Rutledge and designed in the turn-of-the century transitional style, incorporating both Queen Anne and Colonial Revival. Thomas Rutledge was an attorney practicing law in Colusa County. He lived there with his wife Grace Cockscroft until his death in 1942.',
+
+    blogContent2:
+      'The residence is in a rectangular plan with a round corner tower and a conical roof reflecting the Queen Anne influence. The shingle roof has flatted eaves and steep gables with returns. Colonial Revival is seen in the Doric Columns that support the porch sheltering the Main Entrance. Narrow drop siding sheathes the upper and lower stories and a one-story hip-roofed wing is located to the rear. In 1930, this Grand House of Colusa saw an interior renovation and the addition of a half-bath with access from the sun porch.',
+
   },
 ];
 
