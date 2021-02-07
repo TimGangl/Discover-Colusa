8ba895c41babd36213d48a2a69a2b74a8dd2f479
Tim Gangl <tim.gangl30@gmail.com>
2021-02-07 15:32:21 -0800

    working on newsletter component

--- src/components/HomePageText/HomePageText.js
+++ src/components/HomePageText/HomePageText.js
@@ -1,37 +1,43 @@
 import React from 'react';
+import Newsletter from '../Newsletter/Newsletter';
 
 const HomePageText = () => {
 
   return (
     <>
-    <div className="row">
-      <div className="col-2 col-xsm-0"></div>
-      <div className="col-8 col-xsm-12">
-      <h4 className='info text-center mt-5 mb-5'>
-        <strong>
-          <i>Discover Colusa: </i>
-        </strong>
+      <div className="row">
+        <div className="col-2 col-xsm-0"></div>
+        <div className="col-8 col-xsm-12">
+
+          <h4 className='info text-center mt-5 mb-5'>
+            <strong>
+              <i>Discover Colusa: </i>
+            </strong>
         The small town by the big Sacramento River. Colusa is the premiere
         Northern California location for outdoor recreation, hunting, fishing,
-        good food, and a historic downtown with fun places to shop. With a 
+        good food, and a historic downtown with fun places to shop. With a
         growing arts and music scene, beloved
         'street dances', and unique river access, Colusa is home to a collection of extraordinary
         events and activities.
       </h4>
-      <h4 className='info text-center mt-5 mb-5'>
-        Fourth of July fireworks exploding over the river, duck hunting season
-        opened with Colusa Duck Days festival, Christmastime in Colusa marking
+          <h4 className='info text-center mt-5 mb-5'>
+            Fourth of July fireworks exploding over the river, duck hunting season
+            opened with Colusa Duck Days festival, Christmastime in Colusa marking
         the Holidays...<br></br> Colusa is a destination worthy of discovery.
       </h4>
-      <h4 className='info text-center mt-5 mb-5'>
-        Visitors have river access through the Colusa Boat Launch and other
-        points along Main Street. A short hop off of I-5, Colusa is the nexus of
-        Hwy 20 and 45. Guests will easily find hotel accommodations, B&B, and
+          <h4 className='info text-center mt-5 mb-5'>
+            Visitors have river access through the Colusa Boat Launch and other
+            points along Main Street. A short hop off of I-5, Colusa is the nexus of
+            Hwy 20 and 45. Guests will easily find hotel accommodations, B&B, and
         AirBnB options to extend their stay,<br></br> and fully discover Colusa.
       </h4>
+
+          <Newsletter />
+
+        </div>
+        <div className="col-2 col-xsm-0"></div>
       </div>
-      <div className="col-2 col-xsm-0"></div>
-      </div>
+
     </>
   );
 };
---
+++ src/components/Newsletter/Newsletter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Newsletter/Newsletter.css';
+
+function Newsletter() {
+  return (
+    <>
+      <div class="row justify-content-center">
+        <h5 className="subscribe">Subscribe to our Newsletter!</h5>
+      </div>
+
+      <div className="row justify-content-center">
+
+
+      </div>
+    </>
+  )
+}
+
+export default Newsletter;
+
