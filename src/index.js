d9464d020ac7fb8a0d030fb866f9e5674bd3cb04
jedsmit <jedsmit1@gmail.com>
2021-02-21 12:45:12 -0800

    add analytics to index.html remove react-ga code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 
+// ReactGA.initialize('G-VQ6KPJEW4Y'); // add your tracking id here.
+// ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(
   <React.StrictMode>
     <App />
