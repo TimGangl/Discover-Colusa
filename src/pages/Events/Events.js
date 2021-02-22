71ea448e52b79003a9809066e46bb8c4d23c1549
Tim Gangl <tim.gangl30@gmail.com>
2021-02-21 16:02:43 -0800

    working on new updates for events page

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,79 +1,48 @@
 import React from 'react';
-import Navigation from '../../components/Navigation/Navigation';
 import Footer from '../../components/Footer/Footer';
+import Navigation from '../../components/Navigation/Navigation';
 import '../../components/Navigation/Navigation.css';
+import photo3 from '../../images/Colusa Almond Blossom Map.png';
 import photo from '../../images/colusa.jpg';
-import photo2 from '../../images/duck-week.png';
+import photo2 from '../../images/Insta Almond Trail 1.png';
+import photo4 from '../../images/Insta Minis.png';
 import '../Events/Events.css';
 
 
-function Events(){
-  return(
+
+function Events() {
+  return (
     <>
-    <div>
-      <img className="img-fluid" src={photo} alt="colusa field"></img>
-    </div>
-    <Navigation />
-    <div className="row">
-    <div className="col-3"></div>
-    <div className="duck-container col-3">
-      <div className="duck-image text-center"><img className="img-fluid" src={photo2} alt="duck event"></img>
+      <div>
+        <img className="img-fluid" src={photo} alt="colusa field"></img>
       </div>
+      <Navigation />
+      <div className="row">
+        <div className="col-12 text-center mt-5"><h1 className="event-heading1">Colusa Almond Blossom Trail</h1></div>
+        <div className="col-1"></div>
+        <div className="col-lg-3 col-sm-12">
+          <div className="text-center"><img className="img-fluid event-pic-1" src={photo2} alt="Almond Blossom Trail"></img>
+          </div>
+        </div>
+        <div className="col-lg-4 col-sm-12 main event-text"><span className="event-text">Colusa is central to some of the most beautiful almond country in Northern California. Journey here to see the blossoms in full bloom before they disappear! Please click on the map for downloadable copy of directions, and don't forget to eat at one of our fantastic restaurants or shop in a great Colusa store while you're here!</span></div>
+        <div className="col-lg-3 col-sm-12 text-center">
+          <a href="/../public/Colusa Almond Blossom Map.png" download><img className="img-fluid event-pic-2 justify-content-center" src={photo3} alt="map of almond trail">
+          </img></a>
+        </div>
+        <div className="col-1"></div>
       </div>
-      <div className="col-3 main duck-container"><span className="  duck-text">Colusa is world-famous for its superb migratory waterfowl hunting grounds and thousands of hunters flock to this small town every year from October-January hoping to stock their freezers with delicious duck. This year, Colusa restaurants are paying tribute to the last week of Duck Season by adding delicious duck dishes to their menus- for the week of January24th-31st open. Participating restaurants include: </span></div>
-      <div className="col-3"></div>
+      <br>
+      </br>
+      <div className="row">
+        <div className="col-12 text-center mt-5"><h1 className="event-heading2">March of the Minis</h1></div>
+        <div className="col-1"></div>
+        <div className="col-lg-3 col-sm-12"><img className="img-fluid event-pic-3" alt="march of the minis" src={photo4}></img></div>
+        <div className="col-lg-4 col-sm-12 mt-5"><span className="event-text-2">Colusa is inviting Mini Cooper owners to a very special drive-through parade experience along the Sacramento River! March 27th at 11am, Colusa will be opening up it's levee park pathway just for this mini-parade. Parade location starts at 50 Tenth Street (also the location of the Colusa Sacramento River State Park). Please click on map for a downloadable copy of directions.</span></div>
+        <div className="col-lg-3 col-sm-12"><img className="img-fluid event-pic-3" alt="march of the minis" src={photo4}></img></div>
+        <div className="col-1"></div>
       </div>
 
-      <div className="row mt-5">
-        <div className="col-3"></div>
-        <div className="col-3">
-<h2 className="rest-name text-center">Restaurant on the Range</h2>
-<p className="duck-text">221 5th Street</p>
-<a className="duck-text" href="tel:"><p className="duck-text">(530) 619-0191</p></a>
-<p className="duck-text">Duck Tamales, Birria, and Tacos with special seasoning and toppings</p>
-        </div>
-        <div className="col-3">
-<h2 className="rest-name text-center">Market Street Grill</h2>
-<p className="duck-text">415 Market Street</p>
-<a className="duck-text" href="tel:"><p className="duck-text">(530) 458-6100</p></a>
-<p className="duck-text">Smoked Duck Breast over Marquis Potatoes and Raspberry gravy 
-Duck with Crispy Skin Salad, figs, goat cheese, citrus vinagrette
-</p>
-        </div>
-        <div className="col-3"></div>
-      </div>
-      <div className="row mt-5">
-        <div className="col-3"></div>
-        <div className="col-3">
-<h2 className="rest-name text-center">Don Habaneros</h2>
-<p className="duck-text">611 Fremont Street</p>
-<a className="duck-text" href="tel:"><p className="duck-text">(530) 458-3533</p></a>
-<p className="duck-text">Slow braised Duck leg and thigh with semi-sweet chocolate based specialty sauce</p>
-        </div>
-        <div className="col-3">
-<h2 className="rest-name text-center">Rocco's Bar & Grill </h2>
-<p className="duck-text">546 Market Street</p>
-<a className="duck-text" href="tel:"><p className="duck-text">(530) 458-8646</p></a>
-<p className="duck-text">Entree Duck Done Two Ways: Grilled & Duck Leg/Thigh Confit over Wild Mushroom Risotto with Brussel Sprouts and Pancetta. Friday only- Duck Pizza
-</p>
-        </div>
-        <div className="col-3"></div>
-      </div>
-      <div className="row mt-5">
-        <div className="col-3"></div>
-        <div className="col-3">
-<h2 className="rest-name text-center">Las Reinas</h2>
-<p className="duck-text">751 Fremont Street</p>
-<a className="duck-text" href="tel:"><p className="duck-text">(530) 458-7775</p></a>
-<p className="duck-text">Duck with grandmother's secret Mole sauce recipe over rice and beans</p>
-        </div>
-        <div className="col-3">
-
-        </div>
-        <div className="col-3"></div>
-      </div>
-    
-    <Footer />
+      <Footer />
     </>
   )
 }
