8ab7d1936c97b640e41d7081d346f75390d5be9a
Tim Gangl <tim.gangl30@gmail.com>
2021-04-18 16:01:40 -0700

    edits to maps and river pages

--- src/pages/River/River.js
+++ src/pages/River/River.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import '../Visit/Visit.css';
-import Footer from '../../components/Footer/Footer';
+import '../River/River.css';
 import Navigation from '../../components/Navigation/Navigation';
+import Footer from '../../components/Footer/Footer';
 import '../../components/Navigation/Navigation.css';
 import photo from '../../images/colusa.jpg';
-import photo2 from '../../images/hotels-colusa.webp';
-import photo3 from '../../images/food.jpg';
-import photo4 from '../../images/things-to-do.jpg';
+import photo2 from '../../images/duck-hunting.jpg';
+import photo3 from '../../images/bass-pic.jpg';
+import photo4 from '../../images/hiking.jpg';
 import { Card } from 'react-bootstrap';
 
-function Visit(){
+function River(){
   return(
     <>
 <div>
-        <img className="img-fluid" src={photo} alt="colusa field"></img>
-      </div>
-      <Navigation />
-      <div className="row">
-        <div className="col-sm-2"></div>
+  <img className="img-fluid" src={photo} alt="sac-river"></img>
+</div>
+<Navigation />
+<div className="row">
+<div className="col-sm-2"></div>
         <div className="col-sm-8">
-<h1 className="visit-header mt-5 text-center">Visit Beautiful Colusa!</h1>
-<p className="visit-text text-center">Plan your trip to Colusa! Colusa has great natural amenities and services. Find the best places to Shop, Dine, Play and Stay while you’re here!</p>
+<h1 className="river-header text-center mt-5">Sacramento River at Colusa</h1>
+<p className="river-text text-center">Here are just a few of things that you can find to do on or around the Sacramento River while visiting Colusa</p>
 
 <div className="row m-5">
 <div className="col-md-4 col-sm-12">
@@ -29,7 +29,7 @@ function Visit(){
     <Card.Img variant="top" src={photo2} />
   <Card.Body className="river-links">
     <Card.Text>
-      <h5 className="link-header text-center m-5">Hotels In Colusa</h5>
+      <h5 className="link-header text-center m-5">Hunting Maps</h5>
     </Card.Text>
   </Card.Body>
 </Card>
@@ -42,7 +42,7 @@ function Visit(){
   <Card.Img variant="top" src={photo3} />
   <Card.Body className="river-links">
     <Card.Text >
-      <h5 className="link-header text-center m-5">Colusa Restaurants</h5>
+      <h5 className="link-header text-center m-5">Boat Launch & Fishing Info</h5>
     </Card.Text>
   </Card.Body>
 </Card>
@@ -55,7 +55,7 @@ function Visit(){
   <Card.Img variant="top" src={photo4} />
   <Card.Body  className="river-links">
     <Card.Text>
-      <h5 className="link-header text-center m-5 mb-5">Things To Do</h5>
+      <h5 className="link-header text-center m-5 mb-5">Hiking Trails Map</h5>
     </Card.Text>
   </Card.Body>
 </Card>
@@ -67,8 +67,9 @@ function Visit(){
 <div className="col-sm-2"></div>
 </div>
       <Footer />
+
     </>
   )
 }
 
-export default Visit;
+export default River;
