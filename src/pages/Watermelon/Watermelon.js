b8685ff21273ede336e74566791f4675280157bf
TimGangl <tim.gangl30@gmail.com>
2021-06-30 16:52:44 -0700

    added 4th map to page and redeploy

--- src/pages/Watermelon/Watermelon.js
+++ src/pages/Watermelon/Watermelon.js
@@ -3,7 +3,7 @@ import Footer from '../../components/Footer/Footer';
 import Navigation from '../../components/Navigation/Navigation';
 import '../Watermelon/Watermelon.css';
 import photo from '../../images/july-pan.jpg';
-
+import photo2 from '../../images/4th-watermelon.png';
 function Watermelon(){
   return(
     <>
@@ -17,6 +17,10 @@ function Watermelon(){
 <div className="col-sm-8 text-center">
   <h1 className="july-header mt-5
   ">4th of July Watermelon Fest</h1>
+
+<p className="map-text">Click on map for a downloadable version</p>
+
+  <a href={process.env.PUBLIC_URL + '/4th-watermelon.png'} download><img className="fourth-watermelon-pic img-fluid" src={photo2} alt="map for 4th festival"></img></a>
 </div>
 <div className="col-sm-2"></div>
 </div>
